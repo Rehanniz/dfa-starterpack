52f6721d309b6f6ea75a41f5450ea0fa6913b43c
Muhammad Abdullah Shurjeel <99145322+MuhammadAbdullahShurjeel@users.noreply.github.com>
2024-11-16 14:30:33 +0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,8 @@
 Config = {}
 
+-- For Debuging Purpose
+Config.Debug = false
+
 Config.StarterPacks = {
     -- Basic Starter Pack
     ['starter_box'] = {   --- ITEM NAME 
@@ -16,37 +19,23 @@ Config.StarterPacks = {
         money = { type = 'cash', amount = 100 },
 
         givehorse = true,
+        givehorsechance = 50, -- if you want to disable this change it to nil
         horses = {
-             stable = 'valentine',
-             model = 'A_C_Horse_Shire_RavenBlack',
-             gender = 'female',
-             active = true
-    } 
-},
-    
-
-
-    --Example Hunter Pack (No money reward)
-    -- ['starter_box_hunter'] = { -- item name
-    --     name = 'Hunter Pack',
-    --     giveItems = true, -- true / false
-
-    --     items = {
-    --         { name = 'meat', amount = 10 }, --Name: item name | Amount: how much money you want to give
-    --         { name = 'ammo_rifle', amount = 100 }, --Name: item name | Amount: how much money you want to give
-    --         { name = 'weapon_bow', amount = 1 } --Name: item name | Amount: how much money you want to give
-    --     },
-
-    --     giveMoney = false, -- true / false
-    --     money = { type = 'cash', amount = 0 } --Type: cash / bloodmoney / bank / valbank / rhobank / blkbank / armbank | Amount: how much money you want to give
-    -- },
---     givehorse = true, -- true / false
---     horses = {
---          stable = 'valentine', -- colter / vanhorn / rhodes / strawberry / blackwater / tumbleweed / valentine
---          model = 'A_C_Horse_Shire_RavenBlack', -- any horse model https://redlookup.com/peds/?p=1&s=horse
---          gender = 'female', -- Male / Female
---          active = true -- true / false if false they will have to go to stable 
--- } 
+            {
+                stable = 'valentine',
+                model = 'A_C_Horse_Shire_RavenBlack',
+                gender = 'female',
+                active = true
+            },
+            {
+                stable = 'valentine',
+                model = 'A_C_Horse_Shire_RavenBlack',
+                gender = 'female',
+                active = true
+            },
+        } 
+    },
 }
+
 
 
