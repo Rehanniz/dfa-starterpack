058a1fe7e4887fc5f6f996a230dc407326285bf7
Muhammad Abdullah Shurjeel <99145322+MuhammadAbdullahShurjeel@users.noreply.github.com>
2024-11-16 14:30:11 +0500

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,5 +1,7 @@
 local RSGCore = exports['rsg-core']:GetCoreObject()
 
+
+
 local function giveStarterPack(src, packKey)
     local Player = RSGCore.Functions.GetPlayer(src)
     if not Player then return end
@@ -7,8 +9,24 @@ local function giveStarterPack(src, packKey)
     local pack = Config.StarterPacks[packKey]
     if not pack then return end
 
-    if pack.horses then
-        TriggerClientEvent('dfa-starterpack:client:GiveHorse', src, Player, pack.horses.stable, pack.horses.model, pack.horses.gender, pack.horses.active)
+    local chance = math.random(1, 100)
+
+    if Config.Debug then
+        print("Chance: " .. chance)
+    end
+    
+    --- Added Chance System For Horse
+    if pack.givehorsechance ~= nil and chance >= pack.givehorsechance then
+        if pack.horses then
+            -- Added Random Horse Selection From Table If a person wants to add more than one horse...
+            local horsesTable = #pack.horses
+            local randomTableSelector = math.random(1, horsesTable)
+            local horseSelectedTable = pack.horses[randomTableSelector]
+            if Config.Debug then
+                print("HorseTableSelected: " .. randomTableSelector)
+            end
+            TriggerClientEvent('dfa-starterpack:client:GiveHorse', src, Player, horseSelectedTable.stable, horseSelectedTable.model, horseSelectedTable.gender, horseSelectedTable.active)
+        end
     end
 
     Player.Functions.RemoveItem(packKey, 1)
